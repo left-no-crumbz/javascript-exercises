35de621d013ec33de27e139ace697c1077d58cbd
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-24 16:33:38 +0800

    feat: He's BACKKKKK

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let error = (!Number.isFinite(x) || !Number.isFinite(y)) || (x < 0 || y < 0); 
 
+    if (error) {
+        return "ERROR";
+    } else if (y > x) {
+        return ((y+x) * (y))/2;
+    } else if (x > y){
+        return ((y + x) * (x))/2;
+    } 
 };
 
 // Do not edit below this line
