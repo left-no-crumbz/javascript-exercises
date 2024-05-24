4887205d44f27becee3dea60368137e4222a9206
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-24 16:00:17 +0800

    feat: I fell off hard > . >

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    return arr.filter(item => !num.includes(item));
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
