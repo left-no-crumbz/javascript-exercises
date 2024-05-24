d257ed7cf00fe717d10cf94a7a9bbba5cade34e2
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-24 16:59:56 +0800

    feat: Leap year madness

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let divBy4 = year % 4 === 0;
+    let divBy100 = year % 100 === 0;
+    let divBy400 = year % 400 === 0;
+    console.log(divBy4 && !divBy100 && divBy400);
+    return (!divBy100 || divBy400) && divBy4;
 };
 
 // Do not edit below this line
