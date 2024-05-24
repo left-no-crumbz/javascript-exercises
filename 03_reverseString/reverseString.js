701871539396fb3ebf968b38ff2037629e4f2219
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-05-24 12:10:59 +0800

    feat: Add solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = ""
+    let last_index = string.length - 1
+    let length = string.length;
+
+    // SOLUTION 1: w/o using built-in methods but for loop is reversed
+        // for (let i = last_index; i >= 0; i--) {
+        //     reversed += string.charAt(i);
+        // }
+    
+    // SOLUTION 2: for loop is not reversed
+        // for (let i = 0; i < length; i++) {
+        //     reversed = string.charAt(i) + reversed;
+        // }
+
+    // SOLUTION 3: w/o using for loop
+    return string.split("").reverse().join("");
+
 
 };
 
