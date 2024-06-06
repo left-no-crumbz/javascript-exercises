bc5bfeeefd909ac7a5dedcb7c96538700eaed959
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-06 22:25:44 +0800

    feat: two pointer solution palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = (string) => {
+    let palindrome = string.split(" ").join("").split(",").join("").toUpperCase();
+    if (string.endsWith("!") || string.endsWith(".")){
+        const words = palindrome.split("");
+        words.pop();
+        palindrome = words.join("");
+    }
 
-};
+    const length = palindrome.length;
+    // let wordIsPalindrome = false;
+    for (let i = 0; i < length; i++) {
+        if (palindrome[i] !== palindrome[length-1-i]){
+            return false;
+        }
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
