e0b83eb0f204f758afed6722c7d196644e0a1e1f
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-06 15:35:33 +0800

    feat: Answer calc exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce((total, current) => total + current, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((total, current) => total * current);
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (num) => {
+	switch(num) {
+    case 0:
+      return 1;
+    default: {
+      let fac = 1;
+      for (let index = num; index > 0; index--) {
+        fac *= index; 
+        
+      }
+      return fac;
+    }
+  }
 };
 
 // Do not edit below this line
