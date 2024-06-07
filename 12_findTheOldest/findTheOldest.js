8b087b188856f8cd404d7048bfa0c8d5daad50c3
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-07 13:19:36 +0800

    feat: Finish the assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = (arr) => {
+    return arr.reduce((currentPerson, nextPerson) => {
 
-};
+        if (!currentPerson.yearOfDeath){
+            currentPerson.yearOfDeath = new Date().getFullYear();
+        }
+        const currentYearsLived = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        const nextYearsLived = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+
+        if (currentYearsLived > nextYearsLived){
+            return currentPerson;
+        }
+            return nextPerson;
+    })
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
